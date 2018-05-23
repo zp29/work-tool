aff07ef17366400d338359d7470abe9f75f970c6
zp29 <zp.mdzz@gmail.com>
2018-05-23 22:17:22 +0800

    add some db3&mdb files.

--- work-tool-mdbToXls/src/api.js
+++ work-tool-mdbToXls/src/api.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const yargs = require('yargs').argv
 // cmd option
 
@@ -22,56 +21,4 @@ const api = {
   }
 }
 
-=======
-<<<<<<< HEAD
-const yargs = require('yargs').argv
-// cmd option
-
-yargs.file = yargs.file || 'test'
-yargs.src = yargs.src || './src/'
-yargs.dist = yargs.dist || `./data/`
-
-const mdbFile = []
-
-if( typeof(yargs.file) == 'number' ){
-  mdbFile.push(yargs.db3 ? `./mdb/${yargs.file}.db3`  : `./mdb/${yargs.file}.mdb`)
-}else{
-  for( let name of yargs.file.split(',') ){
-    mdbFile.push(yargs.db3 ? `./mdb/${name}.db3`  : `./mdb/${name}.mdb`)
-  }
-}
-
-
-
-
-const api = {
-  mdb: mdbFile,
-  dist: `${yargs.dist}`,
-  html: {
-    main: [],
-    vice: []
-  }
-}
-
-=======
-const yargs = require('yargs').argv
-// cmd option
-
-yargs.file = yargs.file || 'test'
-yargs.src = yargs.src || './src/'
-yargs.dist = yargs.dist || `./data/`
-
-const mdbFile = yargs.db3 ? `./mdb/${yargs.file}.db3`  : `./mdb/${yargs.file}.mdb`
-
-const api = {
-  mdb: mdbFile,
-  dist: `${yargs.dist}`,
-  html: {
-    main: [],
-    vice: []
-  }
-}
-
->>>>>>> 10c34e1a8e37625a3dc7a0397f2e8341bf296fe5
->>>>>>> cb34bfdb4cf6e40dcb83e011d437bcd2925105a9
 module.exports = api
--- work-tool-mdbToXls/src/getdata.js
+++ work-tool-mdbToXls/src/getdata.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const fs = require('fs')
 const sql = require('sql.js')
@@ -25,41 +24,4 @@ exports.getDb3Data = (data) => {
   // });
 
   // return data;
-=======
-<<<<<<< HEAD
-
-const fs = require('fs')
-const sql = require('sql.js')
-const async = require('async')
-
-const api = require('./api.js')
-
-exports.getDb3Data = () => {
-  let data = new Array()
-  async.mapLimit( api.mdb, 1, (name, callback)=>{
-    let db = new sql.Database( fs.readFileSync(name) );
-    db.each('SELECT * FROM Content', function (row) {
-      data.push(row)
-    });
-    callback()
-  })
-  return data;
-=======
-
-const fs = require('fs')
-const sql = require('sql.js')
-
-const api = require('./api.js')
-
-exports.getDb3Data = () => {
-  let file = fs.readFileSync(api.mdb);
-  let db = new sql.Database(file);
-
-  let data = []
-  db.each('SELECT * FROM Content', function (row) {
-    data.push(row)
-  });
-  return data;
->>>>>>> 10c34e1a8e37625a3dc7a0397f2e8341bf296fe5
->>>>>>> cb34bfdb4cf6e40dcb83e011d437bcd2925105a9
 }
