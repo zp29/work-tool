5cd54b27898b38e053faf29e7b1680c6716820db
zp29 <zp29>
2024-07-19 15:10:51 +0800

    目录更新.

--- Raycast/gettoken/src/config.js
+++ Raycast/gettoken/src/config.js
@@ -21,24 +21,24 @@ const environments = [
   };
   
   const encryptedTels = {
-    '10': 'MTg4NjIwODU5OTk=', // 蒋煊
-    '11': 'MTU4Nzg3MDE0NzE=', // 施白云
-    '12': 'MTc3MjEyMDg4MjI=', // 高帅奇
-    '13': 'MTM5MDE4OTQ1Njc=', // 张孜谦
-    '14': 'TVRnMU1ESXdNREE1TWpNPQ', // 林中奇 BUD
-    '20': 'MTgwNTkwNTk4ODI=', // 陈银花
-    '21': 'MTk5NDYyMDQ5ODY=', // 𡈼超凯
-    '22': 'MTU5NjAwMDcwOTE=', // 梁瑷
-    '23': 'MTU4MjE5OTg4OTg=', // 徐磊
-    '1': 'MTMxNjIwNTU3Mjk=', // Grant
-    '2': 'MTUwMDAxNTY1MzU=', // t
-    '3': 'MTc3MTc4OTgyOTg=', // K W
-    '0': 'MTg5OTU3MzE2OTc=' // m
+    '10': 'MTg4NjIwODU5OTk=', 
+    '11': 'MTU4Nzg3MDE0NzE=', 
+    '12': 'MTc3MjEyMDg4MjI=', 
+    '13': 'MTM5MDE4OTQ1Njc=', 
+    '14': 'TVRnMU1ESXdNREE1TWpNPQ', // 
+    '20': 'MTgwNTkwNTk4ODI=', 
+    '21': 'MTk5NDYyMDQ5ODY=', 
+    '22': 'MTU5NjAwMDcwOTE=', 
+    '23': 'MTU4MjE5OTg4OTg=', 
+    '1': 'MTMxNjIwNTU3Mjk=', 
+    '2': 'MTUwMDAxNTY1MzU=', 
+    '3': 'MTc3MTc4OTgyOTg=', // 
+    '0': 'MTg5OTU3MzE2OTc=' 
   };
   const encryptedBbrName = 'YmJyYXVu' //
   
-  const encryptedAuthorization = 'ZDJWaU9uZGxZZz09'; // d2ViOndlYg==
-  const encryptedConfigVerifyUrl = 'YXV0aC9tb2JpbGUvdG9rZW4vc21zP21vYmlsZT1TTVNAe3t9fSZncmFudF90eXBlPW1vYmlsZQ=='; // auth/mobile/token/sms?mobile=SMS@{{}}&grant_type=mobile
+  const encryptedAuthorization = 'ZDJWaU9uZGxZZz09'; //
+  const encryptedConfigVerifyUrl = 'YXV0aC9tb2JpbGUvdG9rZW4vc21zP21vYmlsZT1TTVNAe3t9fSZncmFudF90eXBlPW1vYmlsZQ=='; // 
   
   export { environments, encryptedPass, encryptedFetchRUL, encryptedTels, encryptedAuthorization, encryptedBbrName, encryptedConfigVerifyUrl };
   
