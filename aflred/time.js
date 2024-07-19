32df7c3fee81b49ca742516fb153f132edd80c27
zp29 <zp29>
2024-07-19 14:46:16 +0800

    废弃

--- aflred/time.js
+++ aflred/time.js
@@ -1,58 +1,58 @@
 
-Date.prototype.format = function (format) {
-    //eg:format="yyyy-MM-dd hh:mm:ss";
+// Date.prototype.format = function (format) {
+//     //eg:format="yyyy-MM-dd hh:mm:ss";
 
-    if (!format) {
-        format = "yyyy-MM-dd hh:mm:ss";
-    }
+//     if (!format) {
+//         format = "yyyy-MM-dd hh:mm:ss";
+//     }
 
-    var o = {
-        "M+": this.getMonth() + 1,  // month
-        "d+": this.getDate(),       // day
-        "H+": this.getHours(),      // hour
-        "h+": this.getHours(),      // hour
-        "m+": this.getMinutes(),    // minute
-        "s+": this.getSeconds(),    // second
-        "q+": Math.floor((this.getMonth() + 3) / 3), // quarter
-        "S": this.getMilliseconds()
-    };
+//     var o = {
+//         "M+": this.getMonth() + 1,  // month
+//         "d+": this.getDate(),       // day
+//         "H+": this.getHours(),      // hour
+//         "h+": this.getHours(),      // hour
+//         "m+": this.getMinutes(),    // minute
+//         "s+": this.getSeconds(),    // second
+//         "q+": Math.floor((this.getMonth() + 3) / 3), // quarter
+//         "S": this.getMilliseconds()
+//     };
 
-    if (/(y+)/.test(format)) {
-        format = format.replace(RegExp.$1, (this.getFullYear() + "")
-            .substr(4 - RegExp.$1.length));
-    }
+//     if (/(y+)/.test(format)) {
+//         format = format.replace(RegExp.$1, (this.getFullYear() + "")
+//             .substr(4 - RegExp.$1.length));
+//     }
 
-    for (var k in o) {
-        if (new RegExp("(" + k + ")").test(format)) {
-            format = format.replace(RegExp.$1, RegExp.$1.length == 1
-                ? o[k]
-                : ("00" + o[k]).substr(("" + o[k]).length));
-        }
-    }
+//     for (var k in o) {
+//         if (new RegExp("(" + k + ")").test(format)) {
+//             format = format.replace(RegExp.$1, RegExp.$1.length == 1
+//                 ? o[k]
+//                 : ("00" + o[k]).substr(("" + o[k]).length));
+//         }
+//     }
 
-    return format;
-}
+//     return format;
+// }
 
-import alfy from 'alfy'
-const string = alfy.input ?? ""
+// import alfy from 'alfy'
+// const string = alfy.input ?? ""
 
-let timeString = ''
-if (string === 'now') {
-	timeString = new Date().format("yyyy-MM-dd_HH_mmss")
-} else {
-	timeString = new Date(string).format("yyyy-MM-dd_HH_mmss")
-}
+// let timeString = ''
+// if (string === 'now') {
+// 	timeString = new Date().format("yyyy-MM-dd_HH_mmss")
+// } else {
+// 	timeString = new Date(string).format("yyyy-MM-dd_HH_mmss")
+// }
 
-let items = [''].map(() => {
-	return {
-		title: string,
-		subtitle: timeString,
-		text: {
-		    copy: timeString,
-		    largetype: timeString
-		},
-		arg: timeString
-	}
-})
+// let items = [''].map(() => {
+// 	return {
+// 		title: string,
+// 		subtitle: timeString,
+// 		text: {
+// 		    copy: timeString,
+// 		    largetype: timeString
+// 		},
+// 		arg: timeString
+// 	}
+// })
 
-alfy.output(items)
+// alfy.output(items)
